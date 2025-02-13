62a19526622fd0224fd7e8c3ef8bb23cc201ecd3
Zwielichtig <zwielichtx@gmail.com>
2025-02-13 09:32:19 +0100

    [TASK] #SYN-38 - added fontawesome as npm package

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/ts/app.ts')
+    .addStyleEntry('css', './assets/styles/app.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
