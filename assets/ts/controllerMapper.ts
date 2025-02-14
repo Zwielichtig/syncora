de9bd61f794cb604c8b996ba3df32d898743e4bd
Zwielichtig <zwielichtx@gmail.com>
2025-02-14 09:19:06 +0100

    [TASK] #maintenance - added dynamic imports to TypeScript structure

---
+++ assets/ts/controllerMapper.ts
@@ -0,0 +1,9 @@
+//import { PinController } from './controller/PinController';
+import { CalendarController } from "./controller/CalendarController";
+
+export const controllerMapper: Record<string, any> = {
+//    pin: PinController
+    calendar: CalendarController
+};
+
+// default?
